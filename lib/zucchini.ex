fdfa5649fa6904131124c56848026c6efadadd7f
Jonas Pena <jonasp.dev@gmail.com>
2021-04-01 15:46:35 -0400

    Added Zucchini.start/2 function. Logger emits a message when queue starts.

--- lib/zucchini.ex
+++ lib/zucchini.ex
@@ -1,9 +1,22 @@
 defmodule Zucchini do
   alias Zucchini.{Job, Registry, Queue, Queues}
+  require Logger
 
-  @type queue_name :: String.t | atom | {:global, String.t | atom}
+  @type queue_name :: atom()
   @type task :: {atom, [arg :: term]}
   @type async_opt :: {:reply, true} | {:delay_secs, pos_integer()}
+
+  @spec start(queue_name, []) :: {:ok, pid} | {:error, term()}
+  def start(queue_name, opts \\ []) do
+    with {:ok, pid} <- Queues.start_queue(%{name: queue_name})
+      do
+        Logger.info("Started Queue #{inspect queue_name} at #{inspect(pid)}")
+        {:ok, pid}
+      else
+        {:error, reason} ->
+          Logger.error("Failed to start queue with name #{queue_name}. Reason: #{inspect reason}")
+      end
+  end
 
   @spec async(task, queue_name, [async_opt]) :: Job.t | no_return
   def async(task, queue, opts \\ []) do
@@ -27,9 +40,8 @@ defmodule Zucchini do
 
 
   @doc false
-  def no_queue_error(%Job{queue: {:queue, _} = queue} = job) do
+  def no_queue_error(%Job{queue: _queue_name = queue} = job) do
     "`unable to to find queue: #{inspect queue}, cannot enqueue job #{inspect job}"
   end
 
-  defdelegate start_queue(args), to: Queues
 end
